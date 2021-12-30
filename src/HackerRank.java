32c113e8f890973db60eebb792d113a91d700964
Jackie Dallas <jackie.r.dallas@gmail.com>
2021-12-29 23:29:49 -0600

    completed formatting problem on hackerrank

--- src/HackerRank.java
+++ src/HackerRank.java
@@ -33,8 +33,22 @@ public class HackerRank {
         System.out.println("Int: " + num1);
     }
 
+    public static void formatOutput() {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("================================");
+        for(int i=0;i<3;i++)
+        {
+            String s1=sc.next();
+            int x=sc.nextInt();
+            System.out.printf("%-15s%03d%n", s1, x);
+            // adding spaces affects the output in the console
+        }
+        System.out.println("================================");
+    }
+
     public static void main(String[] args) {
 //        weirdCheck(2);
-        stdinOut();
+//        stdinOut();
+        formatOutput();
     }
 }
